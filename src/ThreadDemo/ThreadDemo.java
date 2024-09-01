727e968611da8aec9ad8f29c490b06c1d10dcef6
“Diego <diegomaradona778@gmail.com>
2024-09-01 14:45:21 +0300

    create new training project

---
+++ src/ThreadDemo/ThreadDemo.java
@@ -0,0 +1,15 @@
+package ThreadDemo;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        SimpleThread simpleThread = new SimpleThread();
+
+        simpleThread.start();
+        try {
+            simpleThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName());
+    }
+}
